96137802496adb44bd261f1aa487b67c3d2921b2
Ashish-simpleCoder <ashishprajapati357753357@gmail.com>
2021-08-12 01:47:03 +0530

    Updates

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,2 +1,3 @@
 self.addEventListener('install',()=>{console.log('service worker installed')})
 self.addEventListener('activate',()=>console.log('service worker activated'))
+self.addEventListener('fetch',(e)=>console.log(e))
